13a07ab8b8b1d12d39daaa231769c28f17ce0cf7
Olin Soffer <olinsoffer@outlook.com>
2017-12-29 08:10:24 +0200

    added conversion

--- server.js
+++ server.js
@@ -19,10 +19,20 @@ let currencyLayerApiKey = '32e6b0b7c2f5e78fd35a229d3e59d3b0';
 let curLayUrl = 'http://www.apilayer.net/api/live?access_key=' + currencyLayerApiKey;
 
 let marketDataUrl = 'http://api.bitcoincharts.com/v1/markets.json';
-const fields = ['symbol', 'currency', 'bid', 'ask', ' latest_trade', 'n_trades', 'high', 'close', 'previous_close', 'volume', 'currency_volume'];
+const fields = ['symbol', 'currency', 'bid', 'ask', ' latest_trade', 'n_trades', 'high', 'close', 'previous_close', 'volume', 'currency_volume', 'conversion'];
+const fieldNames = ['Symbol', 'Currency', 'Bid', 'Ask', 'Latest trade', 'Number of trades', 'High', 'Close', 'Previous close', 'Volume', 'Currency volume', 'Conversion Rate'];
 let counter = 0, marketData;
 
-
+let getConversionData = () => {
+    return new Promise((resolve, reject) => {
+        request(curLayUrl, (err, res, body) => {
+            if (err) {
+                reject(err); return;
+            }
+            resolve(body);
+        });
+    });
+}
 
 let getMarketData = () => {
     return new Promise((resolve, reject) => {
@@ -37,28 +47,41 @@ let getMarketData = () => {
 
 function makeCsv(response) {
     getMarketData()
-        .then((res) => {
-            try {
-                marketData = JSON.parse(res)
-                    .filter((val, index, arr) => {
-                        return (val.bid && val.ask);
-                    });
-                // .map()
-                let csv = json2csv({ data: marketData, fields: fields });
-                console.log(csv);
-                let file = 'file' + counter + '.csv';
-                fs.writeFile('./csv/' + file, csv, (err) => {
-                    if (err) throw err;
-                    console.log('file saved');
-                    // return {file: file}
-                    // let file = 'file' + counter + '.csv';
-                    console.log(file);
-                    counter++;
-                    response.send(file);
+        .then((marketRes) => {
+            getConversionData()
+                .then((conversionRes) => {
+                    console.log(conversionRes);
+                    conversionRes = JSON.parse(conversionRes);
+                    try {
+                        marketData = JSON.parse(marketRes)
+                            .filter((val, index, arr) => {
+                                return (val.bid && val.ask);
+                            });
+                        marketData.forEach((item, index, arr) => {
+                            let currentCurrency = item.currency;
+                            if (currentCurrency !== 'USD' && conversionRes.quotes['USD' + currentCurrency]) {
+                                item.conversion = conversionRes.quotes['USD' + currentCurrency]
+                            } else {
+                                item.conversion = 'no data';
+                            }
+                        });
+                        let csv = json2csv({ data: marketData, fields: fields, fieldNames: fieldNames });
+                        console.log(csv);
+                        let file = 'file' + counter + '.csv';
+                        fs.writeFile('./csv/' + file, csv, (err) => {
+                            if (err) throw err;
+                            console.log('file saved');
+                            // return {file: file}
+                            // let file = 'file' + counter + '.csv';
+                            console.log(file);
+                            counter++;
+                            response.send(file);
+                        });
+                    } catch (err) {
+                        console.error(err);
+                    }
                 });
-            } catch (err) {
-                console.error(err);
-            }
+
         });
 }
 
